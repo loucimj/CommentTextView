68cb9628668bd330314470cf362f80aa218f7df5
Javier Loucim <javier.loucim@globant.com>
2018-03-08 09:30:12 -0300

    Initial Commit

---
+++ CommentTextView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CommentTextView
+//
+//  Created by Javier Loucim on 08/03/2018.
+//  Copyright © 2018 Qeeptouch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
